bca5fc9b69b267eb95a6c77ac63881a7ec11d54d
jerbaroo <jerbaroo.work@pm.me>
2024-06-21 22:52:31 +0100

    reflex-platform binary cache

--- system/system.nix
+++ system/system.nix
@@ -1,5 +1,7 @@
 { stateVersion }: {
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.substituters = [ "https://nixcache.reflex-frp.org" ];
+  nix.settings.trusted-public-keys = [ "ryantrinkle.com-1:JJiAKaRv9mWgpVAz8dwewnZe0AzzEAzPkagE9SP5NWI=" ];
   nixpkgs.config.allowUnfree = true;
   system.stateVersion = stateVersion;
 }
