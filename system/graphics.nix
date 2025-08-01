faacc6122d252f7b4af90e2e03b6f571706641ef
jerbaroo <jerbaroo.work@pm.me>
2025-08-01 11:26:59 +0100

    Cosmic -> Gnome

--- system/graphics.nix
+++ system/graphics.nix
@@ -9,11 +9,11 @@
     enable = true;
     package = inputs.hyprland.packages.${system}.hyprland;
   };
-  services.desktopManager.cosmic.enable = true;
-  services.displayManager.cosmic-greeter.enable = true;
-  # services.xserver = {
-  # enable = true;
-  # desktopManager.gnome.enable = true;
-  # displayManager.gdm.enable = true;
-  # };
+  # services.desktopManager.cosmic.enable = true;
+  # services.displayManager.cosmic-greeter.enable = true;
+  services.xserver = {
+    enable = true;
+    desktopManager.gnome.enable = true;
+    displayManager.gdm.enable = true;
+  };
 }
