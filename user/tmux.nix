6e5f09485326b01a2fb7bc6708d424dc62bef0af
jeremy.barisch.rooney@channable.com <jeremy.barisch.rooney@channable.com>
2025-12-12 22:41:55 +0100

    Fix tmux status line

--- user/tmux.nix
+++ user/tmux.nix
@@ -1,45 +1,66 @@
-{ pkgs, ... }:
+{ accent, palette, pkgs, ... }:
 {
-  # catppuccin.tmux.enable = false;
+  catppuccin.tmux.enable = false;
   programs.tmux = {
+    aggressiveResize = true;
     baseIndex = 1;
     clock24 = true;
     disableConfirmationPrompt = true;
     enable = true;
-    historyLimit = 10000000;
+    escapeTime = 0;
+    historyLimit = 1000000000;
     keyMode = "vi";
     mouse = true;
     secureSocket = true;
     sensibleOnTop = true;
     shortcut = "a";
     shell = "${pkgs.fish}/bin/fish";
+    terminal = "tmux-256color";
     extraConfig = ''
-      set -g escape-time 0
-      set -g default-terminal "tmux-256color"
-      set -ga terminal-overrides ",*:RGB"
+      # Theming.
+      set -as terminal-features ",*:RGB" # True color.
+      set -g pane-active-border-style fg=${palette.${accent}.hex}
+      set -g pane-border-lines heavy
+      set -g pane-border-style fg=${palette.base.hex}
+      set -g message-command-style bg=${palette.base.hex},fg=${palette.text.hex}
+      set -g message-style bg=${palette.base.hex},fg=${palette.text.hex}
+      set -g status-left-style fg=${palette.text.hex}
+      set -g prompt-cursor-colour "${palette.${accent}.hex}"
+      set -g status-right ""
+      set -g status-style bg=default # Transparent status bar background.
+      set -g window-status-separator " "
+      set -g window-status-style fg=${palette.text.hex}
+      set -g window-status-current-style bg=${palette.base.hex},fg="${palette.${accent}.hex} bold"
+
+      # Renumber windows.
+      set -g renumber-windows on
+
+      # Reload configuration.
+      unbind r
+      bind r source-file $HOME/.config/tmux/tmux.conf
+
+      # Clipboard.
       set -g set-clipboard on
+      bind-key -T copy-mode-vi v send-keys -X begin-selection
+      bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle
+      bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
+      unbind -T copy-mode-vi MouseDragEnd1Pane
+
+      # Split panes.
       unbind %
       unbind '"'
       bind | split-window -h -c "#{pane_current_path}"
       bind v split-window -h -c "#{pane_current_path}"
       bind - split-window -v -c "#{pane_current_path}"
       bind s split-window -v -c "#{pane_current_path}"
-      bind h select-pane -L
-      bind j select-pane -D
-      bind k select-pane -U
-      bind l select-pane -R
+
+      # Select panes.
       bind -n M-h select-pane -L
       bind -n M-j select-pane -D
       bind -n M-k select-pane -U
       bind -n M-l select-pane -R
-      unbind r
-      bind r source-file $HOME/.config/tmux/tmux.conf
-      set -g base-index 1
-      set -g pane-base-index 1
-      set-window-option -g pane-base-index 1
-      set-option -g renumber-windows on
-      bind -n S-Left previous-window
-      bind -n S-Right next-window
+
+      # Select windows.
       bind -n M-1 select-window -t 1
       bind -n M-2 select-window -t 2
       bind -n M-3 select-window -t 3
