f11a71aac8a2f9840ca73ffd32e3d96cb0551cea
jeremy.barisch.rooney@channable.com <jeremy.barisch.rooney@channable.com>
2026-01-01 16:31:26 +0100

    Helix adjustments

--- user/helix.nix
+++ user/helix.nix
@@ -1,17 +1,61 @@
 { accent, flavor, lib, pkgs, ... }:
-{
+let 
+  hx-tmux-file-open =
+    # FIXME
+    # Open file in helix and close tmux popup.
+    (pkgs.writeScriptBin "hx-tmux-file-open" ''
+      #!${pkgs.fish}/bin/fish
+
+      echo "Opening $argv[1] in pane $hx_pane" > /tmp/hx_debug.txt
+      tmux send-keys -t "$hx_pane" ":open $argv[1]" C-m
+      # Close the matrix window and the popup.
+      # tmux send-keys -t "$TMUX_PANE" C-c
+      tmux display-popup -C
+    '');
+  hx-tmux-git-open =
+    # Wrapper over tmux-git-open for when in helix.
+    (pkgs.writeScriptBin "hx-tmux-git-open" ''
+      #!${pkgs.fish}/bin/fish
+
+      # TODO simplify this.
+      set -x before_tmux_git_open " \
+        set -gx hx_pane \"$TMUX_PANE\"; \
+        set -gx EDITOR ${hx-tmux-file-open}/bin/hx-tmux-file-open; \
+        set -gx VISUAL ${hx-tmux-file-open}/bin/hx-tmux-file-open; \
+      "
+      tmux-git-open
+    '');
+in {
   home.file.".config/helix/themes/theme.toml".text = ''
     inherits = "catppuccin-${flavor}"
+    "comment" = { fg = "${accent}" }
     "ui.background" = {}
+    "ui.cursor.primary.normal" = { fg = "base", bg = "yellow" }
     "ui.help" = { fg = "text", bg = "base" }
+    "ui.linenr" = { fg = "${accent}" }
+    "ui.linenr.selected" = { fg = "base", bg = "${accent}" }
     "ui.menu" = { fg = "text", bg = "base" }
     "ui.popup" = { fg = "${accent}", bg = "base" }
-    "comment" = { fg = "${accent}" }
+    "ui.statusline.normal" = { fg = "base", bg = "yellow" }
+    "ui.virtual.indent-guide" = { fg = "overlay0" }
+    "ui.virtual.ruler" = { bg = "overlay0" }
   '';
+  home.packages = [ hx-tmux-file-open hx-tmux-git-open ];
   programs.helix = {
-    defaultEditor = true;
+    defaultEditor = false;
     enable = true;
+    languages = {
+      language-server.haskell-language-server = {
+        config = [{haskell.plugin.hlint.globalOn = false; }];
+      };
+    };
     settings = {
+      editor = {
+        line-number = "relative";
+        lsp.display-messages = true;
+        indent-guides.render = true;
+        rulers = [80];
+      };
       keys.normal = {
         "C-h" = "jump_view_left";
         "C-j" = "jump_view_down";
@@ -39,7 +83,11 @@
             d = "diagnostics_picker";
             r = "rename_symbol";
           };
+          f = {
+            s = ":w";
+          };
           g = {
+            # gitu doesn't appear to work in the same way as lazygit does below.
             g = [
               ":write-all"
               ":new"
@@ -49,7 +97,8 @@
               ":buffer-close!"
               ":redraw"
               ":reload-all"
-            ];
+            ];  
+            s = ":run-shell-command '${hx-tmux-git-open}/bin/hx-tmux-git-open %{buffer_name}'";
           };
           l = {
             r = ":lsp-restart";
