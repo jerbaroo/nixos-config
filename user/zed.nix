524035bafbbce469c7071a3a25adc45e1bc43f28
jeremy.barisch.rooney@channable.com <jeremy.barisch.rooney@channable.com>
2026-01-18 11:42:55 +0100

    Enable zed

--- user/zed.nix
+++ user/zed.nix
@@ -1,7 +1,7 @@
 { config, pkgs, wrapGL, ... }:
 {
   programs.zed-editor = {
-    enable = false;
+    enable = true;
     package = (if wrapGL then config.lib.nixGL.wrap else (x: x)) pkgs.zed-editor;
     # package = (if wrapGL then config.lib.nixGL.nixVulkanIntel else (x: x)) pkgs.zed-editor;
   };
